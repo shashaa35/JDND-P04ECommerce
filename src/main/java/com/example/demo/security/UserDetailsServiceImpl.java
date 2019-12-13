3aab29bb60fd6daf532a8330f76a9c3f0defbedf
shasagar <shashaa35@gmail.com>
2019-12-14 00:12:00 +0530

    initial commit

---
+++ src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.demo.security;
+
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import static java.util.Collections.emptyList;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        if(user == null){
+            throw new UsernameNotFoundException(s);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), emptyList());
+    }
+}
